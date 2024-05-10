7f272d2a0183a60a4be92c08011620f21337e232
seongeun223 <jklun080921@gmail.com>
2024-05-10 16:06:13 +0900

    값을 연산하여 출력하기

--- chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section1/literal/Application2.java
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section1/literal/Application2.java
@@ -80,7 +80,21 @@ public class Application2 {
         //주의해야할 점: 숫자로 된 문자열 형태의 값도 + 연산을 했을 때 문자열 합치기가 된다.
         System.out.println("123" + "456"); //123456
 
-        
+        //논리값과 논리값의 연산
+        //논리값끼리의 연산은 모든 연산자 사용이 불가능하다.
+        //System.out.println(true + false);
+        //System.out.println(true - false);
+        //System.out.println(true * false);
+        //System.out.println(true / false);
+        //System.out.println(true % false);
+
+        //논리값과 정수의 연산도 마찬가지로 안 됩니다.
+        //논리값과 실수의 연산도 마찬가지로 X
+        //논리값과 문자의 연산도 X
+        //논리값과 문자열의 연산
+        System.out.println("============ 논리값과 문자열의 값의 연산 ============");
+        System.out.println(true +"a");
+
 
 
 
