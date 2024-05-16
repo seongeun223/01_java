9fce3c6246e3976d3899dcb050bbb73842026a8f
seongeun223 <jklun080921@gmail.com>
2024-05-16 18:10:56 +0900

    commt

--- chap02-operator-lecture-source/src/main/java/com/ohgiraffers/section07/practice/Application1.java
+++ chap02-operator-lecture-source/src/main/java/com/ohgiraffers/section07/practice/Application1.java
@@ -3,30 +3,34 @@ package com.ohgiraffers.section07.practice;
 public class Application1 {
     public static void main(String[] args) {
 
-        System.out.println("산술연산자");
-        int a = 15;
-        int b = 4;
-        System.out.println(a / b);
+//        System.out.println("산술연산자");
+//        int a = 15;
+//        int b = 4;
+//        System.out.println(a / b);
+//
+//        System.out.println();
+//        System.out.println("증감연산자");
+//        int i = 0;
+//        System.out.println("i = " + i);
+//        i++;
+//        System.out.println("i = " + i);
+//
+//        int i = 0;
+//        System.out.println("i = " + i++);
+//        System.out.println("i = " + i);
+//
+//        int i = 0;
+//        System.out.println("i = " + (i + i++));
+//        System.out.println("i = " + i);
+//
+//        int i = 0;
+//        int ii = i++;
+//        System.out.println("ii = " + ii);
+//        System.out.println("i = " + i);
 
-        System.out.println();
-        System.out.println("증감연산자");
-        int i = 0;
-        System.out.println("i = " + i);
-        i++;
-        System.out.println("i = " + i);
-
-        int i = 0;
-        System.out.println("i = " + i++);
-        System.out.println("i = " + i);
-
-        int i = 0;
-        System.out.println("i = " + (i + i++));
-        System.out.println("i = " + i);
-
-        int i = 0;
-        int ii = i++;
-        System.out.println("ii = " + ii);
-        System.out.println("i = " + i);
+        int num1 = 0;
+        int num2 = num1++;
+        System.out.println(num1);
 
 
     }
