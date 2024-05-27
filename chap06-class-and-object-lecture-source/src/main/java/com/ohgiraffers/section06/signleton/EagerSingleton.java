fa169c4a9c32b4ec49e2814e1485f46551481bb6
seongeun223 <jklun080921@gmail.com>
2024-05-27 17:24:45 +0900

    static kindsofvariable initblock

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/signleton/EagerSingleton.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section06.signleton;
+
+public class EagerSingleton {
+
+    // 클래스가 초기화되는 시점에서 인스턴스를 생성한다.
+    private static EagerSingleton earger = new EagerSingleton();
+
+    // 싱글톤 패턴은 생성자 호출을 통해 외부에서 인스턴스 생성을 제한한다.
+    private EagerSingleton() {
+
+    }
+    // public 메소드로 인스턴스를 반환해준다.
+    public static EagerSingleton getInstance() {
+        return earger;
+    }
+}
