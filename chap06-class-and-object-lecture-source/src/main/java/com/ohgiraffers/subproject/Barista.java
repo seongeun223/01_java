8d120af9e68676c18d392dbcc44e9a7b0d445b19
seongeun223 <jklun080921@gmail.com>
2024-05-28 17:22:11 +0900

    cafe

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/subproject/Barista.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/subproject/Barista.java
@@ -11,15 +11,25 @@ public class Barista {
     public Coffee[] makeCoffee(Order order) {
 
         // 재고 차감 처리 - 커피 한 잔에 20g 사용
+        if(coffeeStock >= order.getCount()*20) {
 
+            // 커피 생성
+            Coffee[] coffees = new Coffee[order.getCount()];
+            // 보유량 차감
+            coffeeStock = coffeeStock - order.getCount()*20;
 
-        // 커피 생성
-        Coffee[] coffees = new Coffee[order.getCount()];
+            for(int i = 0; i < coffees.length; i++) {
+                coffees[i] = new Coffee(order.getMenuName(), order.getOption());
+            }
 
-        for(int i = 0; i < coffees.length; i++) {
-            coffees[i] = new Coffee(order.getMenuName(), order.getOption());
+            return coffees;
+
+        } else {
+            System.out.println("[바리스타] : 커피가루 충전해주세요!");
+            return null;
         }
 
-        return coffees;
+
+
     }
 }
