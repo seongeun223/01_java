46e58b12bbb2beb6d749fa98618b885ec244a15f
seongeun223 <jklun080921@gmail.com>
2024-05-28 17:23:17 +0900

    extend

---
+++ chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section01/extend/RacingCar.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section01.extend;
+
+public class RacingCar extends Car {
+// Car 상속은 했지만 모든 메소드 바꿈
+
+    public RacingCar() {
+
+        System.out.println("RacingCar 클래스 기본 생성자 호출");
+
+
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("레이싱카가 전속력으로 질주합니다!");
+    }
+
+    @Override
+    public void soundHorn() {
+
+        // 레이싱카는 경적으로 울리지 않는다.
+        System.out.println("레이싱카는 경적을 울리지 않습니다. (조용...)");
+    }
+
+    @Override
+    public void stop() {
+
+        System.out.println("레이싱카가 멈춥니다.");
+    }
+}
