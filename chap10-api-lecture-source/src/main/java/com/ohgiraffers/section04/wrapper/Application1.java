50833cac2cd4f8777e8a2a5e4f6923ca3d19b781
seongeun223 <jklun080921@gmail.com>
2024-05-29 15:30:29 +0900

    wrapper

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
@@ -23,5 +23,35 @@ public class Application1 {
         * 기본타입에서 레커클래스의 인스턴스로 인스턴스화 하는 것을 박싱(Boxing)
         * 레퍼클래스 타입의 인스턴스를 기본 타입으로 변경하는 것을 언박싱(UnBoxing)
         * */
+
+        int intValue = 20;
+        //Integer boxingNumber1 = new Integer(intValue); // 생성자로 박싱
+        Integer boxingNumber1 = Integer.valueOf(intValue); // static 메소드 이용 // 박싱
+        int unBoxingNumber = boxingNumber1.intValue(); // 언박싱
+
+        /*
+        * 오토 박싱(AutoBoxing)과 오토 언박싱(AutoUnBoxing)
+        * => 박싱과 언박싱이 필요한 상황에서 자바 컴파일러가 자동으로 처리를 해준다.
+        * 이런 자동화된 박싱과 언박싱을 오토박싱, 오토언박싱이라고 한다.
+        * */
+
+        Integer boxingNumber2 = intValue; // 오토박싱
+
+        int unBoxingNumber2 = boxingNumber2; // 오토언박싱
+
+        // Wrapper 클래스의 값 비교
+        int inum = 20;
+        Integer integerNum1 = Integer.valueOf(20); // 박싱
+        Integer integerNum2 = Integer.valueOf(20);
+        Integer integerNum3 = 20; // 오토박싱
+        Integer integerNum4 = 20;
+
+        // 기본타입과 레퍼클래스타입 == 연산으로 비교 가능
+        System.out.println("int와 Integer 비교 : " + (inum == integerNum1));
+        System.out.println("int와 Integer 비교 : " + (inum == integerNum3));
+
+        // 오토박싱과 박싱도 == 연산으로 비교 가능
+        System.out.println("IntegerNum1와 IntegerNum3 비교 : " + (integerNum1 == integerNum3));
+
     }
 }
