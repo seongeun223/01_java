98e83b3953d0dcf2848285e6de4f3b8f44be6c80
seongeun223 <jklun080921@gmail.com>
2024-05-29 13:18:28 +0900

    wrapper

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
@@ -1,2 +1,27 @@
-package com.ohgiraffers.section04.wrapper;public class Application1 {
+package com.ohgiraffers.section04.wrapper;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 기본타입          레퍼클래스
+         * byte             Byte
+         * short            Short
+         * int              Int
+         * long             Long
+         * float            float
+         * double           Double
+         * char             Char
+         * boolean          Boolean
+         * */
+
+        //long price; // -> 0 // null
+
+        /*
+        * 박싱(Boxing)과 언박싱(unBoxing)
+        * 기본타입에서 레커클래스의 인스턴스로 인스턴스화 하는 것을 박싱(Boxing)
+        * 레퍼클래스 타입의 인스턴스를 기본 타입으로 변경하는 것을 언박싱(UnBoxing)
+        * */
+    }
 }
