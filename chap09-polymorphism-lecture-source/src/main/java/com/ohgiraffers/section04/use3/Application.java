14b26e3768890a17d40666db000cf4747dc216b2
seongeun223 <jklun080921@gmail.com>
2024-05-29 10:07:15 +0900

    interface

--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/Application.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/Application.java
@@ -3,10 +3,11 @@ package com.ohgiraffers.section04.use3;
 public class Application {
     public static void main(String[] args) {
 
+        // 다형성
         Dice dice = new SuperDice(); // SuperDice로 변경해도 작동되게
 
-
         Dicer dicer = new Dicer();
+
         dicer.throwDice(dice);
 
 
--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/SuperDice.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use3/SuperDice.java
@@ -2,16 +2,20 @@ package com.ohgiraffers.section04.use3;
 
 public class SuperDice implements Dice{
 
-    private int min;
-    private int max;
+    private int min /* = 1*/;
+    private int max /* = 10*/;
 
     public SuperDice() {
+        // 생성자(추가로 어떤 작업을 하고 싶을 때) 생성
         this.min = 1;
         this.max = 10;
     }
 
     @Override
     public int getNumber() {
-        return (int)(Math.random() * (max - min) + min);
+       // 1~ 10
+        int num = (int) (Math.random() *10) + 1; // 0.xxxx -> 곱하기 n
+        return num;
+        //return (int)(Math.random() * (max - min) + min);
     }
 }
