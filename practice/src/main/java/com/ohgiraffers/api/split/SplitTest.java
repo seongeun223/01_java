eba3328f04dccf17bf94a2e81935a161a8195efe
seongeun223 <jklun080921@gmail.com>
2024-05-30 11:18:41 +0900

    API CSV 데이터 처리

---
+++ practice/src/main/java/com/ohgiraffers/api/split/SplitTest.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.api.split;
+
+import java.util.Arrays;
+
+public class SplitTest {
+    public static void main(String[] args) {
+        String str = "J a v a P r o g r a m ";
+
+        // 문자열 분리
+        String[] strArr = str.split(" ");
+        System.out.println(Arrays.toString(strArr));
+
+        //        for(String s : strArr) {
+//            System.out.println(s);
+
+
+
+        // 문자 배열로 바꾸기
+        // 1) 문자배열을 문자열 배열 크기로 만들기
+        char[] chArr = new char[strArr.length]; // 크기 같게 만들어주기
+
+        // 2) 문자열배열의 값을 문자로 바꿔서, 문자배열에 값 넣기
+        for (int i = 0; i < strArr.length; i++) {
+            chArr[i] = strArr[i].charAt(0);
+        }
+        System.out.println("문자배열 개수 출력 : " + chArr.length);
+        // 문자배열에 값이 잘 들어갔는지 확인
+        System.out.println("문자배열 내용 출력 : " + Arrays.toString(chArr));
+
+        // 문자배열 -> 문자열
+        String result = "";
+        for(char ch : chArr) {
+            result = result + ch;
+        }
+        System.out.println("result = " + result);
+
+        String result2 = new String(chArr);
+        System.out.println("result2 = " + result2);
+
+        // 대문자로 바꾸기
+        result = result.toUpperCase();
+        System.out.println("모두 대문자로 바꾼 result = " + result);
+
+    }
+
+}
+
