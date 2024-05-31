0940ff17fd411a7805a3bfcc13b7a811f78a69f4
seongeun223 <jklun080921@gmail.com>
2024-05-31 15:48:23 +0900

    exception

---
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/ExceptionTest.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section02;
+
+import com.ohgiraffers.section02.exception.MoneyNegativeException;
+import com.ohgiraffers.section02.exception.NotEnoughMoneyException;
+import com.ohgiraffers.section02.exception.PriceNegativeException;
+
+public class ExceptionTest {
+
+    public void checkEnoughMoney(int price, int money) throws Exception {
+
+        if(money > 0) {
+            throw  new MoneyNegativeException("가격은 음수일 수 없습니다.");
+        }
+
+        if(money < 0) {
+            throw new MoneyNegativeException("가지고 있는 돈은 음수일 수 없습니다.");
+        }
+
+        if(money >= price) {
+            System.out.println("상품을 구입하기 위한 금액이 충분합니다!");
+        } else {
+
+            throw new NegativeArraySizeException("가지고 있는 돈보다 상품 가격이 더 비쌉니다");
+        }
+
+
+    }
+
+
+}
